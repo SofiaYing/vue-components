38cf2a97fb10f406f49dbda2e835a24b9b6090f3
sofia <zhangyySmile@126.com>
2019-01-30 12:07:03 +0800

    完善单元测试

--- src/main.js
+++ src/main.js
@@ -20,25 +20,82 @@ import chai from 'chai'
 const expect = chai.expect
 {
   const Constructor = Vue.extend(Button)
-  const button = new Constructor({
+  const vm = new Constructor({
     propsData:{
       icon: 'search'
     }
   })
-  // button.$mount('#test')
-  button.$mount() //直接挂载到内存
-  let useElement = button.$el.querySelector('use')
+  // vm.$mount('#test')
+  vm.$mount() //直接挂载到内存
+  let useElement = vm.$el.querySelector('use')
   console.log(useElement)
   expect(useElement.getAttribute('xlink:href')).to.eq('#icon-search')
+  //清内存
+  vm.$el.remove()
+  vm.$destroy()
 }
 {
   const Constructor = Vue.extend(Button)
-  const button = new Constructor({
+  const vm = new Constructor({
     propsData:{
       icon: 'loading'
     }
   })
-  button.$mount()
-  let useElement = button.$el.querySelector('use')
+  vm.$mount()
+  let useElement = vm.$el.querySelector('use')
   expect(useElement.getAttribute('xlink:href')).to.eq('#icon-loading')
+  vm.$el.remove()
+  vm.$destroy()
+}
+{
+  //挂载到元素上才会进行渲染
+  const div = document.createDlement('div')
+  document.body.appendChild(div)
+
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData:{
+      icon: 'search'
+    }
+  })
+  vm.$mount(div)
+
+  let {order} = window.getComputedStyle(svg)
+  expect(order).to.eq('1')  //CSS属性为字符串
+  vm.$el.remove()
+  vm.$destroy()
+}
+{
+  const div = document.createDlement('div')
+  document.body.appendChild(div)
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData:{
+      icon: 'search',
+      iconPosition: 'right'
+    }
+  })
+  vm.$mount(div)
+  let {order} = window.getComputedStyle(svg)
+  expect(order).to.eq('2') 
+  vm.$el.remove()
+  vm.$destroy() 
+}
+{
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData:{
+      icon: 'search',
+    }
+  })
+  vm.$mount()
+  vm.$on('click',function(){
+    // console.log('1')
+    expect(1).to.equal(1)  //期望这个函数被执行，但是不该如此做
+  })
+  let button = vm.$el
+  button.click()
+
+  vm.$el.remove()
+  vm.$destroy() 
 }
