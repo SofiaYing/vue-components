b3a73cc29104cc39e2b4726cdc2229d43f196465
sofia <zhangyySmile@126.com>
2019-01-31 19:42:38 +0800

    暂时去掉CSS相关的测试用例

--- tests/unit/button.spec.js
+++ tests/unit/button.spec.js
@@ -42,35 +42,35 @@ describe('Button.vue', () => {
     vm.$destroy()
   })
   //运行在node.js环境下，无法获取样式
-  it('icon 默认的 order 是 1', () => {
-    const div = document.createElement('div')
-    document.body.appendChild(div)
-    const Constructor = Vue.extend(Button)
-    const vm = new Constructor({
-    propsData: {
-        icon: 'search',
-    }
-    }).$mount(div)
-    const icon = vm.$el.querySelector('svg')
-    expect(getComputedStyle(icon).order).to.eq('1')
-    vm.$el.remove()
-    vm.$destroy()
-  })
-  it('设置 iconPosition 可以改变 order', () => {
-    const div = document.createElement('div')
-    document.body.appendChild(div)
-    const Constructor = Vue.extend(Button)
-    const vm = new Constructor({
-    propsData: {
-        icon: 'search',
-        iconPosition: 'right'
-    }
-    }).$mount(div)
-    const icon = vm.$el.querySelector('svg')
-    expect(getComputedStyle(icon).order).to.eq('2')
-    vm.$el.remove()
-    vm.$destroy()
-  })
+  // it('icon 默认的 order 是 1', () => {
+  //   const div = document.createElement('div')
+  //   document.body.appendChild(div)
+  //   const Constructor = Vue.extend(Button)
+  //   const vm = new Constructor({
+  //   propsData: {
+  //       icon: 'search',
+  //   }
+  //   }).$mount(div)
+  //   const icon = vm.$el.querySelector('svg')
+  //   expect(getComputedStyle(icon).order).to.eq('1')
+  //   vm.$el.remove()
+  //   vm.$destroy()
+  // })
+  // it('设置 iconPosition 可以改变 order', () => {
+  //   const div = document.createElement('div')
+  //   document.body.appendChild(div)
+  //   const Constructor = Vue.extend(Button)
+  //   const vm = new Constructor({
+  //   propsData: {
+  //       icon: 'search',
+  //       iconPosition: 'right'
+  //   }
+  //   }).$mount(div)
+  //   const icon = vm.$el.querySelector('svg')
+  //   expect(getComputedStyle(icon).order).to.eq('2')
+  //   vm.$el.remove()
+  //   vm.$destroy()
+  // })
   it('点击 button 触发 click 事件', () => {
     // const Constructor = Vue.extend(Button)
     // const vm = new Constructor({
